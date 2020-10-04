20b8db70a2957e0a26c95de191df1174018bffe7
JplsMura <joao.pedro@toxicologiapardini.com.br>
2020-10-04 19:37:20 -0300

    Adicionando Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,63 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+$router->group(['prefix' => '/user'], function () use ($router) {
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+    /*Listagem de registros*/
+    $router->get('/', 'UserController@index');
+
+    $router->post('/create', 'UserController@create');
+
+    $router->put('/update/{id}', 'UserController@update');
+
+    $router->delete('/destroy/{id}', 'UserController@destroy');
+
+});
+
+$router->group(['prefix' => '/endereco'], function () use ($router) {
+
+    /*Listagem de Endereços*/
+    $router->get('/endereco', function () {
+
+    });
+
+    $router->get('/{id_endereco}', function ($id_endereco) {
+        return $id_endereco;
+    });
+});
+
+$router->group(['prefix' => '/cidades'], function () use ($router) {
+
+    /*Listagem de Cidades*/
+    $router->get('/cidades', function () {
+
+    });
+
+    $router->get('/{id_cidade}', function ($id_cidade) {
+        return $id_cidade;
+    });
+});
+
+$router->group(['prefix' => '/estados'], function () use ($router) {
+
+    /*Listagem de Estados*/
+    $router->get('/estados', function () {
+
+    });
+
+    $router->get('/{id_estado}', function ($id_estado) {
+        return $id_estado;
+    });
+});
+
+$router->group(['prefix' => '/estados'], function () use ($router) {
+
+    /*Obter total de usuários cadastrados por cidade*/
+    $router->get('/total', function () {
+
+    });
+
+    /*Obter total de usuários cadastrados por estado*/
+    $router->get('/total', function () {
+
+    });
 });
