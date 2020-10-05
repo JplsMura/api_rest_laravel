748380c67e0e71c357a02a939de47eafa8d26943
JplsMura <joao.pedro@toxicologiapardini.com.br>
2020-10-04 23:03:37 -0300

    Adicionando mais funções ao repositorie

--- app/Repositories/RepositorieUsuario.php
+++ app/Repositories/RepositorieUsuario.php
@@ -3,9 +3,17 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use http\Env\Request;
 
 class RepositorieUsuario
 {
+    public function all()
+    {
+        $all = User::all();
+
+        return $all;
+    }
+
     public function createUsuario($nome, $codendereco, $codcidade, $codestado)
     {
         $createdUsuario = User::create([
@@ -16,5 +24,39 @@ class RepositorieUsuario
         ]);
 
         return $createdUsuario;
+    }
+
+    public function getUserById($id){
+
+        return User::where('id_usuario', $id)->first();
+    }
+
+    public function updateUsuario($user)
+    {
+        try{
+            $userUpdate = User::where('id_usuario', $user['id'])->update([
+                'nome' => $user['nome']
+            ]);
+
+            return $userUpdate;
+        }
+        catch(\Exception $e){
+            return $e->getMessage();
+        }
+    }
+
+    public function destroyUsuario($user)
+    {
+        try {
+
+            $userDestroy = User::where('id_usuario', $user['id'])->delete();
+
+            return $userDestroy;
+
+        }catch (\Exception $e){
+
+            return $e->getMessage();
+
+        }
     }
 }
