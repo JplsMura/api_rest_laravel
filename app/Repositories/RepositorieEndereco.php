97ea8b0bb734f597905e1375608bb9c5b14aa292
JplsMura <joao.pedro@toxicologiapardini.com.br>
2020-10-05 09:34:19 -0300

    RepositorieEndereco com metodo all, e endereco_id

--- app/Repositories/RepositorieEndereco.php
+++ app/Repositories/RepositorieEndereco.php
@@ -7,6 +7,20 @@ use App\Models\Endereco;
 
 class RepositorieEndereco
 {
+    public function all()
+    {
+        $all = Endereco::all();
+
+        return $all;
+    }
+
+    public function endereco_id($id_endereco)
+    {
+        $endereco = Endereco::where('id_endereco', $id_endereco)->first();
+
+        return $endereco;
+    }
+
     public function createEndereco($rua, $numero)
     {
         $createdEndereco = Endereco::create([
