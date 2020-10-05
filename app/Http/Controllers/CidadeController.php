30500d4431caef5ffb45358981d2df3a517ab8bb
JplsMura <joao.pedro@toxicologiapardini.com.br>
2020-10-05 09:51:00 -0300

    Adicionando Controller de Cidade

---
+++ app/Http/Controllers/CidadeController.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Models\Cidade;
+use App\Repositories\RepositorieCidade;
+
+class CidadeController
+{
+    private $cidade;
+
+    public function __construct()
+    {
+        $this->cidade = new RepositorieCidade();
+    }
+
+    public function index()
+    {
+        try {
+
+            $listCidades = $this->cidade->all();
+
+            if($listCidades->filter()->isNotEmpty()){
+                return response()->json([
+                    'status' => true,
+                    'resData' => $listCidades
+                ]);
+            }
+
+            return response()->json([
+                'status' => false,
+                'resData' => 'Nenhum Cidade encontrada',
+            ]);
+
+        }catch (\Exception $e){
+            return response()->json([
+                'status' => false,
+                'resData' => [
+                    'msg' => 'Erro ao buscar cidades!',
+                    'data' => $e->getMessage()
+                ]
+            ]);
+        }
+    }
+
+    public function cidade_especifica($id_cidade)
+    {
+        try {
+
+            $cidade_id = $this->cidade->cidade_id($id_cidade);
+
+            if(!empty($cidade_id)){
+                return response()->json([
+                    'status' => true,
+                    'resData' => $cidade_id
+                ]);
+            }
+
+            return response()->json([
+                'status' => false,
+                'resData' => 'Nenhum Cidade encontrada neste ID',
+            ]);
+
+        }catch (\Exception $e){
+            return response()->json([
+                'status' => false,
+                'resData' => [
+                    'msg' => 'Erro ao buscar cidade!',
+                    'data' => $e->getMessage()
+                ]
+            ]);
+        }
+    }
+}
