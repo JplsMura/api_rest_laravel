799aa5b6c231891d5f6b8666fa90485b5184ed67
JplsMura <joao.pedro@toxicologiapardini.com.br>
2020-10-04 23:04:43 -0300

    Rotas de Usuario

--- routes/web.php
+++ routes/web.php
@@ -13,51 +13,51 @@ $router->group(['prefix' => '/user'], function () use ($router) {
 
 });
 
-$router->group(['prefix' => '/endereco'], function () use ($router) {
-
-    /*Listagem de Endereços*/
-    $router->get('/endereco', function () {
-
-    });
-
-    $router->get('/{id_endereco}', function ($id_endereco) {
-        return $id_endereco;
-    });
-});
-
-$router->group(['prefix' => '/cidades'], function () use ($router) {
-
-    /*Listagem de Cidades*/
-    $router->get('/cidades', function () {
-
-    });
-
-    $router->get('/{id_cidade}', function ($id_cidade) {
-        return $id_cidade;
-    });
-});
-
-$router->group(['prefix' => '/estados'], function () use ($router) {
-
-    /*Listagem de Estados*/
-    $router->get('/estados', function () {
-
-    });
-
-    $router->get('/{id_estado}', function ($id_estado) {
-        return $id_estado;
-    });
-});
-
-$router->group(['prefix' => '/estados'], function () use ($router) {
-
-    /*Obter total de usuários cadastrados por cidade*/
-    $router->get('/total', function () {
-
-    });
-
-    /*Obter total de usuários cadastrados por estado*/
-    $router->get('/total', function () {
-
-    });
-});
+//$router->group(['prefix' => '/endereco'], function () use ($router) {
+//
+//    /*Listagem de Endereços*/
+//    $router->get('/endereco', function () {
+//
+//    });
+//
+//    $router->get('/{id_endereco}', function ($id_endereco) {
+//        return $id_endereco;
+//    });
+//});
+//
+//$router->group(['prefix' => '/cidades'], function () use ($router) {
+//
+//    /*Listagem de Cidades*/
+//    $router->get('/cidades', function () {
+//
+//    });
+//
+//    $router->get('/{id_cidade}', function ($id_cidade) {
+//        return $id_cidade;
+//    });
+//});
+//
+//$router->group(['prefix' => '/estados'], function () use ($router) {
+//
+//    /*Listagem de Estados*/
+//    $router->get('/estados', function () {
+//
+//    });
+//
+//    $router->get('/{id_estado}', function ($id_estado) {
+//        return $id_estado;
+//    });
+//});
+//
+//$router->group(['prefix' => '/estados'], function () use ($router) {
+//
+//    /*Obter total de usuários cadastrados por cidade*/
+//    $router->get('/total', function () {
+//
+//    });
+//
+//    /*Obter total de usuários cadastrados por estado*/
+//    $router->get('/total', function () {
+//
+//    });
+//});
