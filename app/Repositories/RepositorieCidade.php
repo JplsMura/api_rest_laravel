a3adc6af2e62105938ff9a9c1b1c1569ec8502dd
JplsMura <joao.pedro@toxicologiapardini.com.br>
2020-10-05 09:50:44 -0300

    RepositorieCidade com metodo all, e cidade_id

--- app/Repositories/RepositorieCidade.php
+++ app/Repositories/RepositorieCidade.php
@@ -7,6 +7,20 @@ use App\Models\Cidade;
 
 class RepositorieCidade
 {
+    public function all()
+    {
+        $all = Cidade::all();
+
+        return $all;
+    }
+
+    public function cidade_id($id_cidade)
+    {
+        $cidade = Cidade::where('id_cidade', $id_cidade)->first();
+
+        return $cidade;
+    }
+
     public function createCidade($nome, $cep)
     {
         $createdCidade = Cidade::create([
